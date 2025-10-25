ec50f1b56eb114f750a24886155a2ed63bac27ef
Logan House <bonsaiwater@Mac.lan>
2025-10-24 22:09:58 -0500

    fix broken ass script

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env zsh
 
 # Install oh-my-zsh if not present
-if [ ! -d "$HOME/.oh-my-zsh" ]; then    
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   echo "installed oh-my-zsh"
 fi
 
 # Symlink zshrc config
-ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/shell-configs/zsh/.zshrc ~/.zshrc
 echo "established .zshrc symlink "
+
+# Symlnk custom themes and plugins
+src=~/shell-configs/zsh/custom
+dst=~/.oh-my-zsh/custom
+
+cd "$src"
+
+find . -type d -exec mkdir -p "$dst/{}" \;
+find . -type f -exec ln -sf "$src/{}" "$dst/{}" \;
+
 # Symlink custom oh-my-zsh plugins
-ln -sf ~/.dotfiles/zsh/custom ~/.oh-my-zsh/custom
+# ln -sf ~/shell-configs/zsh/custom ~/.oh-my-zsh/custom
+cd ~
 echo "established oh-my-zsh/custom symlink"
 
 echo "Setup complete! Restarting terminal..."
